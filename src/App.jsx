c1e893a859d09d64cf6dca5c0cdc37cbfbb3dde8
Jana-Ahmad <jana.abdullah681@gmail.com>
2024-02-24 18:18:57 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar/Navbar.jsx'
+import About from './components/About/About.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <About />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
